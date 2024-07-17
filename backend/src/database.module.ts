d8e35209139229e81d80012984c2578201bb432a
unknown <HYakimov@vertinitylabs.com>
2024-07-17 10:43:53 +0300

    fixed database module and introduced env files

--- backend/src/database.module.ts
+++ backend/src/database.module.ts
@@ -1,22 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
-import { Score } from './scores/score.entity';
-import { City } from './cities/city.entity';
-import { Country } from './countries/country.entity';
-import { Competition } from './competitions/competition.entity';
+import * as path from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'admin',
-      database: 'mydb',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, `../.env.${process.env.NODE_ENV}`),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true
+      }),
+      inject: [ConfigService],
     }),
   ],
   exports: [TypeOrmModule],
