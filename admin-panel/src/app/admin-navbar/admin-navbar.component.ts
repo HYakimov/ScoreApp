17b4a9b21098a8cf293f2572f8d216dcb35d9fca
unknown <HYakimov@vertinitylabs.com>
2024-07-22 15:13:01 +0300

    fixed navbar

--- admin-panel/src/app/admin-navbar/admin-navbar.component.ts
+++ admin-panel/src/app/admin-navbar/admin-navbar.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: "app-admin-navbar",
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: "./admin-navbar.component.html",
+  styleUrls: ["./admin-navbar.component.css"]
 })
 export class AdminNavbarComponent implements OnInit {
   constructor() { }
--- admin-panel/src/app/app.component.ts
+++ admin-panel/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SharedModule } from './shared/shared.module';
 import { CompetitionComponent } from './competition/competition.component';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, CompetitionComponent],
+  imports: [RouterOutlet, CompetitionComponent, AdminNavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
